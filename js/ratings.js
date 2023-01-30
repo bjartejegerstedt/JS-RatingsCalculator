bb59352d989d58c1f4aaee408ad7193486984769
Bjarte Jegerstedt (bjegerst) <bjegerst@cisco.com>
2023-01-30 16:35:05 +0100

    Completed module

---
+++ js/ratings.js
@@ -0,0 +1,51 @@
+function collect_ratings(){
+
+    let rating = 0;
+    let element = 0;
+
+    let ratings = {
+        count: 0,
+        sum: 0,
+        average: 0
+    };
+
+    const elements = document.querySelectorAll('.rating');
+
+    elements.forEach(function(element) {
+
+        let idStrip= element.id;
+        
+        idStrip = idStrip.replace('star', '');
+
+        rating = parseInt(idStrip);
+
+        ratings.count = ratings.count + parseInt(element.value)
+
+        ratings.sum = ratings.sum + parseInt(element.value) * parseInt(idStrip);
+
+
+    });
+
+if (ratings.count === 0){
+
+    ratings.average = 0;
+
+} else {
+
+    ratings.average = ratings.sum / ratings.count;
+
+};
+
+return ratings;
+    
+};
+
+document.addEventListener("change", function(){
+
+    ratings = collect_ratings();
+
+    let output = document.getElementById("average");
+
+    output.value = ratings.average.toFixed(2);
+
+});
